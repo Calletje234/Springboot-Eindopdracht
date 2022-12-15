b6704371adf8c34da536d5e5109b13c86c201348
Calletje234 <56392128+Calletje234@users.noreply.github.com>
2022-12-15 07:15:15 -0800

    Added post Mapping and Get mapping by id for Child controller

--- SchoolOpdracht/src/main/java/com/example/SchoolOpdracht/controller/ChildController.java
+++ SchoolOpdracht/src/main/java/com/example/SchoolOpdracht/controller/ChildController.java
@@ -2,17 +2,23 @@ package com.example.SchoolOpdracht.controller;
 
 
 import com.example.SchoolOpdracht.dto.ChildDto;
+import com.example.SchoolOpdracht.helpers.Util;
 import com.example.SchoolOpdracht.model.Child;
 import com.example.SchoolOpdracht.service.ChildService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/childs")
 public class ChildController {
     private final ChildService service;
+    private Util util;
 
     public ChildController(ChildService c) {
         this.service = c;
@@ -23,5 +29,22 @@ public class ChildController {
         return ResponseEntity.ok(service.getAllChildren());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ChildDto> getChildById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getChildById(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> createChild(@Valid @RequestBody ChildDto childDto, BindingResult br) {
+        if(br.hasErrors()) {
+            return new ResponseEntity<>(util.createErrorMessage(br).toString(), HttpStatus.BAD_REQUEST);
+        }
+        Long createdId = service.createChild(childDto);
+
+        URI uri = URI.create(
+                ServletUriComponentsBuilder.fromCurrentContextPath().path("/children/" + createdId).toUriString());
+        return ResponseEntity.created(uri).body("Child Created");
+
+    }
 
 }
