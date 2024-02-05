4c9380d035c980769897405b8d612f11b5edfcf5
Calvin van Beek <calvinvanbeek123@outlook.com>
2024-02-05 15:46:27 +0100

    Encrypted password

--- SchoolOpdracht/src/main/resources/data.sql
+++ SchoolOpdracht/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into roles(rolename) values ('USER'), ('ADMIN'), ('BEHANDELAAR');
-insert into users(username, password) values ('ADMIN', '$29po12A#');
+insert into users(username, password) values ('ADMIN', '$2a$10$nFpBJtQSwdneAM67MrPv5O54Q0T4GZyTIiyfCzcdkQvFyh4zQ2qWK');
 insert into users_roles(users_username, roles_rolename) values ('ADMIN', 'ADMIN');
